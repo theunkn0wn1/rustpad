53befc376526d21a9baf74164456c04bdb9d25aa
joshua salzedo <thHunkn0WNd@gmail.com>
2020-05-25 22:34:11 -0700

    Began work on generating rust code

---
+++ rustpad/generator/__main__.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+import typing
+from pathlib import Path
+from typing import List
+
+import attr
+import click
+import jinja2
+import toml
+
+env = jinja2.Environment(loader=jinja2.PackageLoader("generator"), trim_blocks=True, lstrip_blocks=True)
+
+
+@attr.dataclass(frozen=True, slots=True)
+class Event:
+    code: int = attr.ib(validator=attr.validators.instance_of(int), converter=int)
+    name: str = attr.ib(validator=attr.validators.instance_of(str))
+
+    def events(self) -> typing.List[Event]:
+        return [self]
+
+
+@attr.dataclass(frozen=True)
+class ButtonEvent:
+    pressed: Event
+    released: Event
+
+
+@attr.dataclass(frozen=True)
+class AxisEvent:
+    action: Event
+
+
+def axis(name: str, key: int) -> typing.Set[Event]:
+    return {Event(name=f"{name}Motion", code=key)}
+
+
+def button(name: str, key: int) -> typing.Set[Event]:
+    return {
+        Event(name=f"{name}Pressed", code=key),
+        Event(name=f"{name}Released", code=key),
+    }
+
+
+@click.command("build")
+@click.argument("path")
+def cli(path: str):
+    target = Path(path)
+    del path
+    if not target.exists():
+        raise click.Abort(f"{target.absolute()} does not exist.")
+
+    data = toml.load(target)
+    axis_events: List[AxisEvent] = [AxisEvent(action=Event(name=f"{base}Action", code=key)) for key, base in data['axes'].items()]
+
+    button_events:List[ButtonEvent] = []
+
+    for key, base in data['buttons'].items():
+        button_events.append(ButtonEvent(
+            pressed=Event(name=f"{base}Pressed", code=key),
+            released=Event(name=f"{base}Released", code=key)
+        ))
+    event_names = [event.action.name for event in axis_events]
+    event_names.extend(event.pressed.name for event in button_events)
+    event_names.extend(event.released.name for event in button_events)
+
+    print(event_names)
+    template = env.get_template("enum.jinja")
+    print(template.render(events=sorted(event_names)))
+
+
+cli()
